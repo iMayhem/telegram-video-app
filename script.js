d3e585e7f1b4dca2cdc7e87a77ebd66054d42444
iMayhem <67825237+iMayhem@users.noreply.github.com>
2025-05-10 22:55:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // List of your Telegram video links (replace these with your actual links)
 const videoLinks = [
   { title: "Lecture 1", url: "https://t.me/c/2642042750/1?embed=1" },
-  { title: "Lecture 2", url: "https://t.me/c/2642042750/2?embed=2" },
-  { title: "Lecture 3", url: "https://t.me/c/2642042750/3?embed=3" },
-  { title: "Lecture 4", url: "https://t.me/c/2642042750/4?embed=4" },
-  { title: "Lecture 5", url: "https://t.me/c/2642042750/5?embed=5" },
-  { title: "Lecture 6", url: "https://t.me/c/2642042750/6?embed=6" },
-  { title: "Lecture 7", url: "https://t.me/c/2642042750/7?embed=7" },
-  { title: "Lecture 8", url: "https://t.me/c/2642042750/8?embed=8" },
-  { title: "Lecture 9", url: "https://t.me/c/2642042750/9?embed=9" },
-  { title: "Lecture 10", url: "https://t.me/c/2642042750/10?embed=10" },
-  { title: "Lecture 11", url: "https://t.me/c/2642042750/11?embed=11" },
+  { title: "Lecture 2", url: "https://t.me/c/2642042750/2?embed=1" },
+  { title: "Lecture 3", url: "https://t.me/c/2642042750/3?embed=1" },
+  { title: "Lecture 4", url: "https://t.me/c/2642042750/4?embed=1" },
+  { title: "Lecture 5", url: "https://t.me/c/2642042750/5?embed=1" },
+  { title: "Lecture 6", url: "https://t.me/c/2642042750/6?embed=1" },
+  { title: "Lecture 7", url: "https://t.me/c/2642042750/7?embed=1" },
+  { title: "Lecture 8", url: "https://t.me/c/2642042750/8?embed=1" },
+  { title: "Lecture 9", url: "https://t.me/c/2642042750/9?embed=1" },
+  { title: "Lecture 10", url: "https://t.me/c/2642042750/10?embed=1" },
+  { title: "Lecture 11", url: "https://t.me/c/2642042750/11?embed=1" },
 ];
 
 // Check if user is already logged in
